cd2ddb96f153b72aed5daa0104c95df77d95626c
MaeganW <maeganwomble@gmail.com>
2019-07-11 13:01:05 -0500

    added ChooseDeviceScreen class tests

---
+++ TestsEditMode/UITests/ViewModelTests/ChooseDeviceScreenTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ChooseDeviceScreenTests
+    {
+        [Test]
+        public void test_cds_class_methods()
+        {
+            ChooseDeviceScreen cds = new ChooseDeviceScreen();
+            int num = 99;
+            
+            Assert.AreEqual(cds.Increment(num), 100);
+        }
+    }
+}
